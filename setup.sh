006db599799d83088895d66d67ba34e72e7ccee1
s278577 <vitocianchini@hotmail.it>
2021-02-13 19:48:27 +0100

    refactor

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# TO BE CONFIGURED
+##########
+transmission_dir="/mnt/grosso"
+##########
+
 apt-get update 
 apt-get install -y ntfs-3g screen transmission-daemon samba at  weechat  youtube-dl git
 apt-get install -y build-essential libssl-dev libffi-dev python3-dev 
@@ -22,7 +27,7 @@ curl -fsSL get.docker.com | sh
 curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | bash -s -- -m raspberrypi4
 
 #configure transmission
-transmission_dir="/mnt/grosso"
+
 service transmission-daemon stop
 python3 replace_json.py $transmission_dir
 service transmission-daemon restart
