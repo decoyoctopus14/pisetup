784bf54c30e6fb2d660a7cf954b6c0bc6f0f57b1
decoyoctopus14 <kenny310789@gmail.com>
2021-04-24 16:26:16 +0200

    Update replace_json.py

--- replace_json.py
+++ replace_json.py
@@ -7,7 +7,7 @@ transmission_dir=sys.argv[1]
 filename = '/etc/transmission-daemon/settings.json'
 with open(filename, 'r') as f:
     data = json.load(f)
-    data['download-dir'] = transmission_dir+"/transmission"
+    data['download-dir'] = transmission_dir+"/transmission/downloaded"
     data["incomplete-dir"]=transmission_dir+"/transmission/incomplete"
     data["incomplete-dir-enabled"]=True
     data["rpc-whitelist"]="*.*.*.*"
